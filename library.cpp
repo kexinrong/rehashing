992ceedce652dc3d2765f1f00de050236312a3de
Kexin Rong <kexinrongg@gmail.com>
2018-09-04 17:09:07 -0700

    Parser

--- library.cpp
+++ library.cpp
@@ -3,18 +3,20 @@
 #include <iostream>
 #include <Eigen/Dense>
 #include "alg/naiveKDE.h"
-#include "utils/kernel.h"
+#include "utils/utils.h"
 #include "utils/expkernel.h"
 
 
 int main() {
     // code
     std::cout << "Naive KDE" << std::endl;
-    MatrixXd data = MatrixXd::Random(10,3);
+    MatrixXd data = Utils::readFile("resources/test.csv", false, 4, 3);
+    std::cout << data << std::endl;
     Expkernel kernel;
     kernel.initialize(new double[3]{1,1,1}, 3);
     naiveKDE naive(data, &kernel);
     double result = naive.query(data.row(0));
     std::cout << result << std::endl;
+
     return 0;
 }
--- utils/utils.h
+++ utils/utils.h
@@ -5,7 +5,12 @@
 #ifndef HBE_UTILS_H
 #define HBE_UTILS_H
 
+#include <Eigen/Dense>
 #include <math.h>
+#include "utils/CSVparser.h"
+
+using namespace parser::csv;
+using Eigen::MatrixXd;
 
 class Utils {
 
@@ -16,6 +21,30 @@ public:
     static double expRelVar(double mu) { return E1 / sqrt(mu); }
     static double gaussRelVar(double mu) { return E2 / sqrt(mu); }
     static double randomRelVar(double mu) { return 1 / mu; }
+
+    static MatrixXd readFile(string filename, bool ignoreHeader, int n, int d) {
+        std::ifstream f(filename);
+        CsvParser parser(f);
+
+        MatrixXd data(n, d);
+        bool firstRow = true;
+        int i = 0;
+        for (auto& row : parser) {
+            if (ignoreHeader && firstRow) {
+                firstRow = false;
+                continue;
+            }
+            int j = 0;
+            for (auto& field : row) {
+                data(i, j) = std::stof(field);
+                j += 1;
+                if (j == d) { break; }
+            }
+            i += 1;
+            if (i == n) { break; }
+        }
+        return data;
+    }
 
 
 };
