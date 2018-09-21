a347224f72bad81bc930f621b556eda3780db3ff
Kexin Rong <kexinrongg@gmail.com>
2018-09-20 22:31:15 -0700

    Figtree on covtype

--- figtree/samples/realdata.cpp
+++ figtree/samples/realdata.cpp
@@ -40,7 +40,7 @@
 #include <math.h>   // for abs
 #include "figtree.h"
 #include <time.h>       /* clock_t, clock, CLOCKS_PER_SEC */
-
+#include <algorithm>    // std::max
 
 // This file only shows examples of how the figtree() function can be used.
 //
@@ -60,7 +60,7 @@ void readFile(std::string filename, bool ignoreHeader, int n, int startCol, int 
 
     std::string line;
     std::string delim = ",";
-    while (std::getline(infile, line) && i < n) {
+    while (std::getline(infile, line)) {
         if (ignoreHeader && i == 0) {
             i += 1;
             continue;
@@ -70,17 +70,40 @@ void readFile(std::string filename, bool ignoreHeader, int n, int startCol, int 
         int j = 0;
         while (end != std::string::npos && j <= endCol) {
             if (j >= startCol) {
-                data[i * dim + j - startCol] = atof(line.substr(start, end - start).c_str());
+                if (ignoreHeader) {
+                    data[(i-1) * dim + j - startCol] = atof(line.substr(start, end - start).c_str());
+                } else {
+                    data[i * dim + j - startCol] = atof(line.substr(start, end - start).c_str());
+                }
             }
             start = end + delim.length();
             end = line.find(delim, start);
             j += 1;
         }
         i += 1;
+        if (ignoreHeader && i == n + 1) {
+            break;
+        } else if (!ignoreHeader && i == n) {
+            break;
+        }
     }
     infile.close();
 }
 
+void fitCube(double* data, int n, int d) {
+    for (int j = 0; j < d; j++) {
+        double mmin = data[j];
+        double mmax = data[j];
+        for (int i = 1; i < n; i ++) {
+            mmin = std::min(data[i * d + j], mmin);
+            mmax = std::max(data[i * d + j], mmax);
+        }
+        double range = mmax - mmin;
+        for (int i = 0; i < n; i ++) {
+            data[i * d + j] = (data[i * d + j] - mmin) / range;
+        }
+    }
+}
 
 
 int main()
@@ -89,7 +112,7 @@ int main()
     int d = 10;
 
     // The number of targets (vectors at which gauss transform is evaluated).
-    int M = 10;
+    int M = 50000;
 
     // The number of sources which will be used for the gauss transform.
     int N = 50000;
@@ -112,20 +135,22 @@ int main()
     // For example, below N = 20 and d = 7, so there are 20 rows, each
     // a 7-dimensional sample.
     double x[N * d];
-    readFile("../../resources/covtype.data", true, N, 0, 9, &x[0]);
+    readFile("../../resources/covtype.data", false, N, 0, 9, &x[0]);
+    fitCube(&x[0], N, d);
 
     // The target array.  It is a contiguous array, where
     // ( y[j*d], y[j*d+1], ..., y[j*d+d-1] ) is the jth d-dimensional sample.
     // For example, below M = 10 and d = 7, so there are 10 rows, each
     // a 7-dimensional sample.
     double y[M * d];
-    readFile("../../resources/covtype.data", true, M, 0, 9, &y[0]);
+    for (size_t i = 0; i < M * d; i ++) { y[i] = x[i]; }
 
     // The weight array.  The ith weight is associated with the ith source sample.
     // To evaluate the Gauss Transform with the same sources and targets, but
     // different sets of weights, add another row of weights and set W = 2.
-    double q[N];
-    for (int i = 0; i < N; i ++) { q[i] = 1; }
+    double *q = new double[N];
+    for (size_t i = 0; i < N; i ++) { q[i] = 1; }
+    //memset(q, 1, sizeof(double) * N );
 
     // Number of weights.  For each set of weights a different Gauss Transform is computed,
     // but by giving multiple sets of weights at once some overhead can be shared.
@@ -134,23 +159,15 @@ int main()
     // allocate array into which the result of the Gauss Transform will be stored for each
     // target sample.  The first M elements will correspond to the Gauss Transform computed
     // with the first set of weights, second M elements will correspond to the G.T. computed
-    // with the second set of weights, etc.
+    // with the second set of weights, etc.ha
     double * g_auto = new double[W*M];
     double * g_sf = new double[W*M];
-    double * g_sf_tree = new double[W*M];
-    double * g_ifgt_u = new double[W*M];
-    double * g_ifgt_tree_u = new double[W*M];
-    double * g_ifgt_nu = new double[W*M];
-    double * g_ifgt_tree_nu = new double[W*M];
+
 
     // initialize all output arrays to zero
     memset( g_auto        , 0, sizeof(double)*W*M );
     memset( g_sf          , 0, sizeof(double)*W*M );
-    memset( g_sf_tree     , 0, sizeof(double)*W*M );
-    memset( g_ifgt_u      , 0, sizeof(double)*W*M );
-    memset( g_ifgt_tree_u , 0, sizeof(double)*W*M );
-    memset( g_ifgt_nu     , 0, sizeof(double)*W*M );
-    memset( g_ifgt_tree_nu, 0, sizeof(double)*W*M );
+
 
     //
     // RECOMMENDED way to call figtree().
@@ -178,44 +195,31 @@ int main()
     // evaluate gauss transform using direct (slow) method
     figtree( d, N, M, W, x, h, q, y, epsilon, g_sf, FIGTREE_EVAL_DIRECT );
 
-    // evaluate gauss transform using direct method with approximate nearest neighbors
-    figtree( d, N, M, W, x, h, q, y, epsilon, g_sf_tree, FIGTREE_EVAL_DIRECT_TREE );
-
-    // evaluate gauss transform using FIGTREE (truncated series), estimating parameters with and without
-    //   the assumption that sources are uniformly distributed
-    figtree( d, N, M, W, x, h, q, y, epsilon, g_ifgt_u, FIGTREE_EVAL_IFGT, FIGTREE_PARAM_UNIFORM, 1 );
-    figtree( d, N, M, W, x, h, q, y, epsilon, g_ifgt_nu, FIGTREE_EVAL_IFGT, FIGTREE_PARAM_NON_UNIFORM, 1 );
-
-    // evaluate gauss transform using FIGTREE (truncated series), estimating parameters with and without
-    //   the assumption that sources are uniformly distributed
-    figtree( d, N, M, W, x, h, q, y, epsilon, g_ifgt_tree_u, FIGTREE_EVAL_IFGT_TREE, FIGTREE_PARAM_UNIFORM, 1 );
-    figtree( d, N, M, W, x, h, q, y, epsilon, g_ifgt_tree_nu, FIGTREE_EVAL_IFGT_TREE, FIGTREE_PARAM_NON_UNIFORM, 1 );
-
     // compute absolute error of the Gauss Transform at each target and for all sets of weights.
+    double avg_error = 0;
+    double avg_relerror = 0;
     for( int i = 0; i < W*M; i++) {
+       avg_error += fabs(g_auto[i] - g_sf[i]);
+        avg_relerror +=  fabs(g_auto[i] - g_sf[i]) / g_sf[i];
         g_auto[i]         = fabs(g_auto[i]        -g_sf[i]);
-        g_sf_tree[i]      = fabs(g_sf_tree[i]     -g_sf[i]);
-        g_ifgt_u[i]       = fabs(g_ifgt_u[i]      -g_sf[i]);
-        g_ifgt_nu[i]      = fabs(g_ifgt_nu[i]     -g_sf[i]);
-        g_ifgt_tree_u[i]  = fabs(g_ifgt_tree_u[i] -g_sf[i]);
-        g_ifgt_tree_nu[i] = fabs(g_ifgt_tree_nu[i]-g_sf[i]);
     }
+    avg_error /= W*M;
+    avg_relerror /= W*M;
+    printf("Average absolute error: %f\n", avg_error);
+    printf("Average relative error: %f\n", avg_relerror);
 
     // print out results for all six ways to evaluate
-    printf("Results:\n");
-    printf("Direct result auto error    sf-tree error ifgt-u error  ifgt-nu error  ifgt-tree-u e ifgt-tree-nu error\n");
-    for( int i = 0; i < W*M; i++ )
-        printf("%13.4f %13.4e %13.4e %13.4e %13.4e %13.4e %13.4e\n",
-               g_sf[i], g_auto[i], g_sf_tree[i], g_ifgt_u[i], g_ifgt_nu[i], g_ifgt_tree_u[i], g_ifgt_tree_nu[i] );
-    printf("\n");
+//    printf("Results:\n");
+//    printf("Direct result | auto error\n");
+//    for( int i = 0; i < W*M; i++ )
+//        printf("%14.4f %14.4e\n", g_sf[i], g_auto[i]);
+////        printf("%13.4f %13.4e %13.4e %13.4e %13.4e %13.4e %13.4e\n",
+////               g_sf[i], g_auto[i], g_sf_tree[i], g_ifgt_u[i], g_ifgt_nu[i], g_ifgt_tree_u[i], g_ifgt_tree_nu[i] );
+//    printf("\n");
 
     // deallocate memory
+    delete [] q;
     delete [] g_auto;
     delete [] g_sf;
-    delete [] g_sf_tree;
-    delete [] g_ifgt_u;
-    delete [] g_ifgt_nu;
-    delete [] g_ifgt_tree_u;
-    delete [] g_ifgt_tree_nu;
     return 0;
 }
