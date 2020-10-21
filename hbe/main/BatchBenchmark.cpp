401b1f99970d2cc8cb8444f4c7ee5b94f4ebeb7f
Kexin Rong <kexinrongg@gmail.com>
2020-10-20 22:42:28 -0700

    Remove sketch table 3 scale example

--- hbe/main/BatchBenchmark.cpp
+++ hbe/main/BatchBenchmark.cpp
@@ -4,10 +4,6 @@
  *          RS: RS on a reservoir of random samples
  *          Uniform HBE: HBE on a reservoir of random samples
  *          Sketch HBE: HBE on a sketch produced by HBS
- *          Sketch (3 scales) HBE: HBE on HBS, where in each hash bucket of the HBS,
- *                                 we store 3 data samples (one from each weight scale).
- *                                 Comparing to storing 1 sample per bucket (default),
- *                                 this option takes slightly longer but is more accurate.
  *
  *
  *      The relative runtime of HBE and RS can be controlled by changing the "sample_ratio"
@@ -63,12 +59,6 @@ int main(int argc, char *argv[]) {
     std::cout << "Sketch Table Init: " <<
         std::chrono::duration_cast<std::chrono::seconds>(t2-t1).count() << std::endl;
 
-    t1 = std::chrono::high_resolution_clock::now();
-    SketchHBE sketch4(data.X_ptr, tables, data.w, data.k, 3, data.kernel);
-    t2 = std::chrono::high_resolution_clock::now();
-    std::cout << "Sketch Table Init (3 scales): " <<
-        std::chrono::duration_cast<std::chrono::seconds>(t2-t1).count() << std::endl;
-
     double cnt = 0;
     for (auto& t : hbe.tables) {
         cnt += t.bucket_count;
@@ -87,11 +77,9 @@ int main(int argc, char *argv[]) {
         hbe.totalTime = 0;
         rs.totalTime = 0;
         sketch.totalTime = 0;
-        sketch4.totalTime = 0;
         vector<double> hbe_error;
         vector<double> sketch_error;
         vector<double> rs_error;
-        vector<double> sketch_scale_error;
 
         for(int j = 0; j < data.M; j++) {
             int idx = j * 2;
@@ -108,24 +96,19 @@ int main(int argc, char *argv[]) {
 
             double hbe_est = hbe.query(q, data.tau, samples);
             double sketch_est = sketch.query(q, data.tau, samples);
-            double sketch_scale_est = sketch4.query(q, data.tau, samples);
             double rs_est = rs.query(q, data.tau, int(samples * data.sample_ratio));
             hbe_error.push_back(relErr(hbe_est, exact_val));
             rs_error.push_back(relErr(rs_est, exact_val));
             sketch_error.push_back(relErr(sketch_est,exact_val));
-            sketch_scale_error.push_back(relErr(sketch_scale_est, exact_val));
         }
 
         std::cout << "Uniform HBE total time: " << hbe.totalTime / 1e9 << std::endl;
         std::cout << "Sketch HBE total time: " << sketch.totalTime / 1e9 << std::endl;
-        std::cout << "Sketch (3 scales) HBE total time: " << sketch4.totalTime / 1e9 << std::endl;
         std::cout << "RS Sampling total time: " << rs.totalTime / 1e9 << std::endl;
         printf("Uniform HBE relative error: %f, %f, %f\n",
                 dataUtils::getAvg(hbe_error), dataUtils::getStd(hbe_error), dataUtils::getMax(hbe_error));
         printf("Sketch HBE relative error:  %f, %f, %f\n",
                dataUtils::getAvg(sketch_error), dataUtils::getStd(sketch_error), dataUtils::getMax(sketch_error));
-        printf("Sketch (3 scales)  HBE relative error:  %f, %f, %f\n",
-               dataUtils::getAvg(sketch_scale_error),dataUtils:: getStd(sketch_scale_error), dataUtils::getMax(sketch_scale_error));
         printf("RS relative error:  %f, %f, %f\n",
                dataUtils::getAvg(rs_error), dataUtils::getStd(rs_error), dataUtils::getMax(rs_error));
     }
